082ab0d32c2b7af8eefa4b0131c8411879ce6e71
sm praneeth kumar <smpraneethkumar23@gmail.com>
2025-11-11 13:02:57 +0530

    testing the git commits

---
+++ src/com/praneeth/dsa/linear_search/Dummay_class.java
@@ -0,0 +1,8 @@
+package com.praneeth.dsa.linear_search;
+
+public class Dummay_class {
+
+    public static void main(String[] args) {
+        System.out.println("this is a dummay text ");
+    }
+}
--- src/com/praneeth/dsa/linear_search/sample_line.java
+++ src/com/praneeth/dsa/linear_search/sample_line.java
@@ -4,5 +4,18 @@ public class sample_line {
 
     public static void main(String[] args) {
         System.out.println("linear search created");
+
+        int a = 1;
+        int b =2;
+        int c =3;
+
+//        int value =;
+
+        System.out.println("if( "+c+" < "+a+" ){");
+        System.out.println("System.out.println(''hello world'');");
+        System.out.println("}");
+        System.out.println(21%10);
+
+
     }
 }
