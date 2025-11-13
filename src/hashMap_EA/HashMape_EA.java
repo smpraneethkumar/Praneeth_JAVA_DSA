358c0e2214b79bfd5f728fd41d99532c7e3d4cc9
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-11-13 18:04:41 +0530

    added hashmap files

---
+++ src/hashMap_EA/HashMape_EA.java
@@ -0,0 +1,42 @@
+package hashMap_EA;
+
+import java.util.HashMap;
+
+public class HashMape_EA {
+
+    public static void main(String[] args) {
+
+        HashMap<String,Integer> has =new  HashMap<>();
+
+        String arr[] = {"praneeth","ankit","praneeth","harsha","harsha"};
+
+        for(int i =0;i<arr.length;i++) {
+
+            String n = arr[i];
+
+            if(has.containsKey(n)) {
+                int per= has.get(n);
+                has.put(n, per+1);
+            }else {
+                has.put(n, 1);
+            }
+
+//			System.out.println(has.get(n));
+        }
+
+        for(String Boom:has.keySet()) {
+            System.out.println(Boom+" " +has.get(Boom));
+        }
+
+
+
+//		has.put(3, 4);
+//
+//		int per = has.get(3);
+//		System.out.println(per);
+//
+//		has.put(3, per+2);
+//
+//		System.out.println(has.get(3));
+    }
+}
---
+++ src/hashMap_EA/Majority_Element_169_EA.java
@@ -0,0 +1,54 @@
+package hashMap_EA;
+
+import java.util.HashMap;
+
+public class Majority_Element_169_EA {
+
+    public static void main(String[] args) {
+        int nums[] = {3,2,3};
+
+        int i = nums.length/2;
+//		System.out.println(i);
+//		System.out.println(me(nums));
+        System.out.println(leet(nums));
+    }
+
+    static  int me(int [] nums) {
+
+        HashMap <Integer,Integer> has =new HashMap<>();
+        int ans =0;
+        for(int i =0;i<nums.length;i++) {
+
+            int val = nums[i];
+
+            if(has.containsKey(val)) {
+                return ans =val;
+            }else {
+                has.put(val, 1);
+            }
+        }
+
+        return ans ;
+    }
+    static int leet(int[] nums) {
+
+        HashMap <Integer, Integer> lee = new HashMap <>();
+
+        for(int i =0;i<nums.length;i++) {
+            int key = nums[i];
+            lee.put(key, lee.getOrDefault(key,0)+1);
+        }
+
+        int ans = 0;
+        int n = nums.length/2;
+
+        for(int j:lee.keySet()) {
+            int val = lee.get(j);
+
+            if(val>n) {
+                ans = j;
+            }
+        }
+        return ans;
+    }
+}
