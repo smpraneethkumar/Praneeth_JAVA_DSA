ce77b532a358b03944644c98c8c786a3b1f8b4ac
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-11-14 19:29:04 +0530

    added patterns

---
+++ src/strings_Kunal/Goal_Parser_Interpretation_1678_KK.java
@@ -0,0 +1,34 @@
+package strings_Kunal;
+
+public class Goal_Parser_Interpretation_1678_KK {
+
+    public static void main(String[] args) {
+        String  command = "(al)G(al)()()G";
+
+//        System.out.println(command.charAt(2));
+//        for(int )
+//            int a =1;
+        int n = command.length();
+        String ans ="";
+        for(int i =0;i<n-1;i++){
+
+            char ch1 = command.charAt(i);
+            char ch2 = command.charAt(i+1);
+
+            if(ch1 == 'G'){
+                ans += 'G';
+                continue;
+            }
+            if(ch1 == '(' && ch2 == ')'){
+                ans += 'o';
+                continue;
+            }
+            if(ch1 == '(' && ch2 == 'a' ){
+                ans += "al";
+                continue;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
