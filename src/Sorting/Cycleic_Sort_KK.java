6a2ea4912dcfe66bbabe2074137e7d3719a12909
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-11-16 22:39:16 +0530

    created sorting added cycle,bubble sortings

---
+++ src/Sorting/Cycleic_Sort_KK.java
@@ -0,0 +1,50 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Cycleic_Sort_KK {
+    public static void main(String[] args) {
+
+        int arr [] = {3,5,2,1,4};
+
+
+        sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void sort(int[] arr) {
+        // TODO Auto-generated method stub
+
+        int i =0;
+
+
+        while(i<arr.length) {
+            int correct = arr[i]-1;
+
+            if(arr[i] != arr[correct]) {
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+
+
+
+
+    }
+
+
+
+    private static void swap(int[] arr, int first, int second){
+
+
+        int temp = arr[first];
+
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+    }
