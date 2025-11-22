b05192c0483d7a57b5552d2b30af2ed703bcbf50
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-11-22 18:04:40 +0530

    Q 1343 is done

--- src/Regex/sample.java
+++ src/Regex/sample.java
@@ -2,15 +2,29 @@ package Regex;
 import java.util.regex.*;
 public class sample {
 
-    String regex = "for\\s*\\(\\s*int\\s+i\\s*=\\s*(-?\\d+)\\s*;\\s*i\\s*([<>!=]+)\\s*(-?\\d+)\\s*;\\s*i\\s*(\\+\\+|--|\\+=\\s*\\d+|-=?\\s*\\d+)\\s*\\)";
-    String line = "for (int i = 0; i < 10; i+=2)";
+    public static void main(String[] args) {
 
-    Pattern pattern = Pattern.compile(regex);
-    Matcher matcher = pattern.matcher(line);
+        String regex = "for\\s*\\(\\s*int\\s+i\\s*=\\s*(-?\\d+)\\s*;\\s*i\\s*([<>!=]+)\\s*(-?\\d+)\\s*;\\s*i\\s*(\\+\\+|--|\\+=\\s*\\d+|-=?\\s*\\d+)\\s*\\)";
+        String line = "for (int i = 0; i < 10; i+=2)";
 
-//        if (matcher.find()) {
-//        System.out.println("Matched!");
-//    } else {
-//        System.out.println("Not matched");
-//    }
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.find()) {
+        System.out.println("Matched!");
+    } else {
+        System.out.println("Not matched");
+    }
+
+        String name = "sdfsdfs";
+
+        name = "praneeth";
+
+        System.out.println(name
+           );
+
+    }
+
+
+
 }
---
+++ src/slidingWindowTwoPointer/Number_of_sub_arrays_of_size_k_and_average_greater_than_or_equal_to_threshold_1343_M_EA.java
@@ -0,0 +1,38 @@
+package slidingWindowTwoPointer;
+
+public class Number_of_sub_arrays_of_size_k_and_average_greater_than_or_equal_to_threshold_1343_M_EA {
+
+    public static void main(String[] args) {
+
+        int arr[] = {2,2,2,2,5,5,5,8};
+
+        int k = 3;
+        int threshold = 4;
+
+        System.out.println(numOfSubarrays(arr,k,threshold));
+    }
+    public static  int numOfSubarrays(int[] arr, int k, int threshold) {
+
+        int n = arr.length;
+        int temp =0;
+        int ans=0;
+        int l =0;
+
+
+        for(int i =0;i<n;i++){
+            temp += arr[i];
+
+            if(i-l == k){
+                temp = temp - arr[l];
+                l++;
+            }
+            if(i-l+1 == k){
+                if(temp/k>=threshold){
+                    ans++;
+                }
+            }
+
+        }
+        return ans;
+    }
+}
