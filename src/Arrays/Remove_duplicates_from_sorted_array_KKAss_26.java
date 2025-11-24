823ba34f48245068bc6c8431787016220f013a84
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-11-24 19:07:55 +0530

    26 is done by tap vid

--- src/Arrays/Remove_duplicates_from_sorted_array_KKAss_26.java
+++ src/Arrays/Remove_duplicates_from_sorted_array_KKAss_26.java
@@ -6,38 +6,35 @@ public class Remove_duplicates_from_sorted_array_KKAss_26 {
 
     public static void main(String[] args) {
 
-//        int [] nums = {0,1,2,2};
-        int [] nums = {0,0,1,1,1,2,2,3,3,4};
+        int [] nums = {0,1,2,2};
+//        int [] nums = {0,0,1,1,1,2,2,3,3,4};
 
-//        System.out.println(removeDuplicates(nums));
-
-        int ans = 0;
-
-        for(int i =0;i<nums.length;i++){
-
-            if(nums[i] == i ){
-                int temp = nums[i];
-                nums[i] = temp;
-                ans++;
-            }else {
-                nums[i] =0;
-            }
-
-            if(nums[i+1] == i ){
-                int temp = nums[i];
-                nums[i] = temp;
-                ans++;
-            }else {
-                nums[i] =0;
-            }
-        }
-        System.out.println(Arrays.toString(nums));
-        System.out.println(ans);
+        System.out.println(removeDuplicates(nums));
+//        int ans = 0;
+//        for(int i =0;i<nums.length;i++){
+//            if(nums[i] != i ){
+//                ans++;
+//            }else {
+//                nums[i] =0;
+//            }
+//        }
     }
 
-//     static  public int removeDuplicates(int[] nums) {
-//
-//
-//    }
+     static  public int removeDuplicates(int[] nums) {
+
+        int rd =0;
+        int n = nums.length;
+
+        for(int i =0;i<n;i++){
+
+            if(nums[rd] != nums[i]){
+                    rd++;
+                nums[rd] = nums[i];
+
+            }
+        }
+        return rd+1;
+
+    }
 
 }
