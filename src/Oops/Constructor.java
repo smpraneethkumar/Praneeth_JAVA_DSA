f54231e1c4e2ab8578fd96bf7e44672faaa7bfdb
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-11-27 23:07:52 +0530

    oops

---
+++ src/Oops/Constructor.java
@@ -0,0 +1,21 @@
+package Oops;
+
+
+class  Demo{
+    String name;
+
+      Demo(String n){
+          n = "dkjncfjkcf";
+        System.out.println(n);
+    }
+}
+public class Constructor {
+
+
+
+    public static void main(String[] args) {
+
+        Demo c = new Demo("eee");
+
+    }
+}
