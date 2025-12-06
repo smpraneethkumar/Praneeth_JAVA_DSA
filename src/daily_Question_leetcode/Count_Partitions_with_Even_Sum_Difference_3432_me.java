4610281b1e079e585f8c456fb2e3f39a65f6cf19
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-12-07 00:07:26 +0530

    3136 is done

---
+++ src/daily_Question_leetcode/Count_Partitions_with_Even_Sum_Difference_3432_me.java
@@ -0,0 +1,17 @@
+package daily_Question_leetcode;
+
+public class Count_Partitions_with_Even_Sum_Difference_3432_me {
+
+    public static void main(String[] args) {
+
+        int nums[] = {10,10,3,7,6};
+
+        System.out.println(countPartitions(nums));
+
+    }
+    public static int countPartitions(int[] nums) {
+        int ans = 0;
+        return ans;
+
+    }
+}
