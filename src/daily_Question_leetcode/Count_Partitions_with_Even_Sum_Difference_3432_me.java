c1a60efc3ec01757a1c17c9257c2ac1df6e7a0bc
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2025-12-08 01:09:15 +0530

    3432 is pending

--- src/daily_Question_leetcode/Count_Partitions_with_Even_Sum_Difference_3432_me.java
+++ src/daily_Question_leetcode/Count_Partitions_with_Even_Sum_Difference_3432_me.java
@@ -4,7 +4,7 @@ public class Count_Partitions_with_Even_Sum_Difference_3432_me {
 
     public static void main(String[] args) {
 
-        int nums[] = {10,10,3,7,6};
+        int nums[] = {10,10,3,7,6,4};
 
         System.out.println(countPartitions(nums));
 
