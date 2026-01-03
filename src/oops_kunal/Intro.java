335a0ee181c91906cc9a6f901ccf6ac380b050b5
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-01-04 00:01:35 +0530

    class,object,constructors

--- src/oops_kunal/Intro.java
+++ src/oops_kunal/Intro.java
@@ -10,11 +10,20 @@ public class Intro {
 //
 //        System.out.println(Arrays.toString(a));
 
-        Student student1 = new Student();
+        Student student1 = new Student(23,"praneeth",50.00f);
+        Student student2 = new Student(23,"praneeth kumar",50.00f);
 
-        student1.name = "fsdfsdf";
+//        student1.name = "fsdfsdf";
+//        student1.rno = 01;
+//        student1.marks = 23.5f;
 
-        System.out.println(student1.name);
+        System.out.println(student2.name);
+        System.out.println(student1.rno);
+        System.out.println(student1.marks);
+
+         final int see = 12;
+//see =13;
+        System.out.println(see);
     }
 
 
@@ -22,6 +31,12 @@ public class Intro {
 class Student{
 
     int rno;
-    String name = "sdfsdfsdf";
+    String name ;//= "sdfsdfsdf";
     float marks;
+
+    Student(int rno,String name,float marks){
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
+    }
 }
