f7c9bf89de8fd6508e5458f260b043d43cf30df6
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-01-06 18:14:11 +0530

    power of three is solved

---
+++ src/daily_Question_leetcode/Largest_3_same_digit_number_in_string_2264_06_01_2026.java
@@ -0,0 +1,27 @@
+package daily_Question_leetcode;
+
+public class Largest_3_same_digit_number_in_string_2264_06_01_2026 {
+
+
+    public static void main(String[] args) {
+        String num = "6777133339";
+
+        System.out.println(largestGoodInteger(num));
+    }
+    static public String largestGoodInteger(String num) {
+
+        String ans = "";
+        int n = num.length();
+        for(int i =0;i<n;i++){
+
+//            if(num.charAt(i)){
+//
+//            }
+
+        }
+//        if(ans.length() == 3){
+
+        return ans;
+//        }
+    }
+}
