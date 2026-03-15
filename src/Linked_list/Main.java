0f9ef951730b23e5e60363ebd015a09ca78c2a9c
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-03-16 00:15:49 +0530

    linked list

---
+++ src/Linked_list/Main.java
@@ -0,0 +1,16 @@
+package Linked_list;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(12);
+
+        list.display();
+
+    }
+}
